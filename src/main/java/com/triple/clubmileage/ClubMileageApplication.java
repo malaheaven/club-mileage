7e7714d291979bf1d3298403144db0ed24aaafb1
malaheaven <yunhg95@gmail.com>
2022-07-02 13:50:46 +0900

    [ADD] PROJECT SETUP

---
+++ src/main/java/com/triple/clubmileage/ClubMileageApplication.java
@@ -0,0 +1,13 @@
+package com.triple.clubmileage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubMileageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClubMileageApplication.class, args);
+    }
+
+}
