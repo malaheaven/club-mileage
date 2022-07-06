1663e45a2930847d218b7861db89df1a0c99cf1d
malaheaven <yunhg95@gmail.com>
2022-07-06 14:40:33 +0900

    [MOD] UserReviewHistory user_review_history_idx Index 필드 추가

--- src/main/java/com/triple/clubmileage/domain/document/UserReviewHistory.java
+++ src/main/java/com/triple/clubmileage/domain/document/UserReviewHistory.java
@@ -1,6 +1,9 @@
 package com.triple.clubmileage.domain.document;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -9,12 +12,18 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 import org.springframework.data.mongodb.core.mapping.FieldType;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Getter
-@CompoundIndex(name = "user_review_history_idx", def = "{review_id : 1, place_id: 1}")
-@Document
-public class UserReviewHistory {
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@CompoundIndex(name = "user_review_history_idx", def = "{place_id: 1, review_id : 1, user_id: 1}", unique = true)
+@Document(collection = "USER_REVIEW_HISTORY")
+public class UserReviewHistory implements Serializable {
+
+    private static final long serialVersionUID = 3550288393866215542L;
 
     @Id
     @Field(targetType = FieldType.OBJECT_ID)
