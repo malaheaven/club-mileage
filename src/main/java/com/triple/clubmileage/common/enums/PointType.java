e410344d7e8ee10f574408189715b9ea20daf0ba
malaheaven <yunhg95@gmail.com>
2022-07-06 14:55:25 +0900

    [ADD] PointType Enum 추가

--- src/main/java/com/triple/clubmileage/common/enums/PointType.java
+++ src/main/java/com/triple/clubmileage/common/enums/PointType.java
@@ -1,10 +1,16 @@
 package com.triple.clubmileage.common.enums;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public enum PointType {
 
-    REVIEW
+    REVIEW("REVIEW");
+
+    String label;
 
 }
