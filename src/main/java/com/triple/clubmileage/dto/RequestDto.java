7ea30417b63555bad7b04f17dcf10533995969c2
malaheaven <yunhg95@gmail.com>
2022-07-06 14:59:49 +0900

    [ADD] 리뷰와 관련된 RequestDto 개발

---
+++ src/main/java/com/triple/clubmileage/dto/RequestDto.java
@@ -0,0 +1,43 @@
+package com.triple.clubmileage.dto;
+
+import com.triple.clubmileage.common.enums.PointType;
+import com.triple.clubmileage.common.enums.review.ReviewType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class RequestDto {
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Review {
+        @NotNull
+        private PointType type;
+
+        @NotNull
+        private ReviewType action;
+
+        @NotNull
+        private String reviewId;
+
+        private String content;
+
+        @Builder.Default
+        private List<String> attachedPhotoIds = new ArrayList<>();
+
+        @NotNull
+        private String userId;
+
+        @NotNull
+        private String placeId;
+    }
+
+}
